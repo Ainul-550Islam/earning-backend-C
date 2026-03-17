f82df0935427301aa4d9303f1de9f3b933991632
Ainul Islam <ainul10islam100@gmail.com>
2026-03-17 12:41:03 +0600

    fix: fix social_django indentation

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'social_django',
     'django.contrib.sites',
 
     # Third-party apps
