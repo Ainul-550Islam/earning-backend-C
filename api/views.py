9e645e756ed5c3164c0029a085ce7b0c56349d00
Ainul Islam <ainul10islam100@gmail.com>
2026-03-17 20:07:53 +0600

    feat: add email sending to forgot_password

--- api/views.py
+++ api/views.py
@@ -134,8 +134,14 @@ def forgot_password(request):
         token = default_token_generator.make_token(user)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         reset_url = f"https://earning-frontend-v2.vercel.app/reset-password/{uid}/{token}/"
-        # TODO: Add email sending later
-        # For now return reset URL directly
+        from django.core.mail import send_mail
+        send_mail(
+            subject='Password Reset Request',
+            message=f'Click the link to reset your password:\n\n{reset_url}',
+            from_email=None,
+            recipient_list=[email],
+            fail_silently=False,
+        )
         return Response({'message': 'Password reset email sent!', 'reset_url': reset_url})
     except User.DoesNotExist:
         return Response({'message': 'Password reset email sent!'})
