951b6b24423d36e6a984d31e35a4ce115ecb2b1a
Ainul Islam <ainul10islam100@gmail.com>
2026-03-18 11:47:15 +0600

    fix: use env DJANGO_SETTINGS_MODULE in create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -1,11 +1,14 @@
 import os
 import django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE', 'config.settings.base'))
 django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='AinulIslam').exists():
-    User.objects.create_superuser('AinulIslam', 'ainul10islam100@gmail.com', '99998888')
-    print('Superuser created!')
-else:
-    print('Superuser already exists!')
+try:
+    if not User.objects.filter(username='AinulIslam').exists():
+        User.objects.create_superuser('AinulIslam', 'ainul10islam100@gmail.com', '99998888')
+        print('Superuser created!')
+    else:
+        print('Superuser already exists!')
+except Exception as e:
+    print(f'Superuser creation skipped: {e}')
